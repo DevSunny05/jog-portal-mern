b276b9b7018e5b6037cd005c09d855b6f0660c85
sarang <sarang.office01@gmail.com>
2023-05-10 14:54:04 +0530

    update job api created

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -11,12 +11,43 @@ export const createJobController=async(req,res,next)=>{
     const job=await jobModel.create(req.body)
     res.status(201).json({job})
 }
-
+// get jobs controller
 export const getAllJobsController=async(req,res,next)=>{
     const jobs=await jobModel.find({createdBy:req.user.userId})
     res.status(200).json({
         totlaJobs:jobs.length,
         jobs
 
+    })
+}
+
+// update jobs || put/patch
+export const updateJobController=async(req,res,next)=>{
+    const {id}=req.params
+    const {company,position,}=req.body
+
+    if(!company || !position){
+        next("Please provide credientials")
+    }
+
+    const job=await jobModel.findOne({_id:id})
+
+    if(!job){
+        next(`no job found with this ${id}`)
+    }
+
+    if(!req.user.userId === job.createdBy.toString()){
+        next('You are not authorize to update')
+        return  
+    }
+
+    const updatedJob=await jobModel.findOneAndUpdate({_id:id},req.body,{
+        new:true,
+        runValidators:true
+    })
+
+    res.status(200).json({
+        success:true,
+        updatedJob
     })
 }
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import userAuth from '../middlewares/authMiddleware.js'
-import { createJobController, getAllJobsController } from '../controllers/jobsController.js'
+import { createJobController, getAllJobsController, updateJobController } from '../controllers/jobsController.js'
 
 const router=express.Router()
 
@@ -9,5 +9,8 @@ router.post('/create-job',userAuth,createJobController)
 
 // get all jobs || get
 router.get('/get-jobs',userAuth,getAllJobsController)
+
+// update jobs || put
+router.put('/update-job/:id',userAuth,updateJobController)
 
 export default router
