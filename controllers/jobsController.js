b5b103af67f221784c49b4cbaa38b0bd637198fa
sarang <sarang.office01@gmail.com>
2023-06-03 16:26:06 +0530

    pagination done

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -53,12 +53,23 @@ export const getAllJobsController=async(req,res,next)=>{
             queryResult=queryResult.sort('-position')
         }
 
+
+        // paginations
+        const page=Number(req.query.page) || 1
+        const limit =Number(req.query.limit) || 10
+        const skip=(page -1)*limit
+        queryResult=queryResult.skip(skip).limit(limit)
+
+        const totalJobs=await jobModel.countDocuments(queryResult)
+        const numOfPages=Math.ceil(totalJobs/limit)
+
         const jobs=await queryResult
     
     // const jobs=await jobModel.find({createdBy:req.user.userId})
     res.status(200).json({
-        totlaJobs:jobs.length,
-        jobs
+        totalJobs,
+        jobs,
+        numOfPages
 
     })
 }
