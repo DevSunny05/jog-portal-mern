4af07188c21dfb779d5a6d5d06dd8b55d39e99f0
sarang <sarang.office01@gmail.com>
2023-06-10 13:06:08 +0530

    make reusable register and login components

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InputForm from '../components/InputForm'
+import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        try {
+            console.log(email,password)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
   return (
-    <div>LoginPage</div>
+    <>
+    <div className="form-container">
+    <h2 className="m-4">Login Form</h2>
+      <form className="card p-4" onSubmit={handleSubmit}>
+          <InputForm htmlFor='email' labelText='Email:' type={'email'} value={email} handleChange={(e)=>setEmail(e.target.value) } name={'email'}  />
+          <InputForm htmlFor='password' labelText='Password:' type={'password'} value={password} handleChange={(e)=>setPassword(e.target.value) } name={'password'}  />
+
+          <div className="d-flex">
+              <p className="me-4">New User? <Link to='/register'>Register</Link></p>
+              <button type="submit" className="btn btn-primary">Login</button>
+          </div>
+       
+      </form>
+    </div>
+  </>
   )
 }
 
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,78 +1,38 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import InputForm from "../components/InputForm";
 const RegisterPage = () => {
-    // const [name,setName]=useState('')
-    // const [lastName,setLastName]=useState('')
-    // const [email,setEmail]=useState('')
-    // const [password,setPassword]=useState('')
-
-    const [values,setValues]=useState([{
-        name:'',
-        lastName:'',
-        email:'',
-        password:''
-    }])
+    const [name,setName]=useState('')
+    const [lastName,setLastName]=useState('')
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
 
 
     const handleSubmit=(e)=>{
         e.preventDefault()
         try {
-            console.log(values)
+            console.log(name,lastName,email,password)
         } catch (error) {
             console.log(error)
         }
         
     }
-
-    const handleChange=(e)=>{
-        const value=e.target.value
-        setValues({...values,
-        [e.target.name]:value
-        })
-    }
     
   return (
     <>
       <div className="form-container">
+        <h2 className="m-4">Register Form</h2>
         <form className="card p-4" onSubmit={handleSubmit}>
+            <InputForm htmlFor='name' labelText='Name:' type={'text'} value={name} handleChange={(e)=>setName(e.target.value) } name={'name'}  />
+            <InputForm htmlFor='lastName' labelText='LastName:' type={'text'} value={lastName} handleChange={(e)=>setLastName(e.target.value) } name={'lastName'}  />
+            <InputForm htmlFor='email' labelText='Email:' type={'email'} value={email} handleChange={(e)=>setEmail(e.target.value) } name={'email'}  />
+            <InputForm htmlFor='password' labelText='Password:' type={'password'} value={password} handleChange={(e)=>setPassword(e.target.value) } name={'password'}  />
 
-          <div className="mb-1">
-            <label htmlFor="name" className="form-label">
-              Name
-            </label>
-            <input type="text" className="form-control" name="name" value={values.name} onChange={handleChange}  />
-          </div>
-
-          <div className="mb-1">
-            <label htmlFor="lastName" className="form-label" >
-              Last Name
-            </label>
-            <input type="text" className="form-control" name='lastName' value={values.lastName} onChange={handleChange}  />
-          </div>
-
-          <div className="mb-1">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input type="email" className="form-control" name="email" value={values.email}  onChange={handleChange}  />
-          </div>
-          <div className="mb-1">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input type="password" className="form-control" name="password" value={values.password} onChange={handleChange}   />
-          </div>
-
-          
-          <div className="d-flex justify-content-between mt-2 ">
-            <p className="me-2">
-              Already Registerd?<Link to="/login">Login</Link>
-            </p>
-
-            <button type="submit" className="btn btn-primary">
-              Register
-            </button>
-          </div>
+            <div className="d-flex">
+                <p className="me-4">Already Registerd? <Link to='/login'>Login</Link></p>
+                <button type="submit" className="btn btn-primary">Register</button>
+            </div>
+         
         </form>
       </div>
     </>
